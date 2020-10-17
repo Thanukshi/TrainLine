b513ea9aacf4a7ae44f5a914ebe0fc5c79c9ab9c
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-10-18 02:08:02 +0530

    Train Line (Thanukshi)

--- app/src/main/java/com/example/trainline/LoginActivity.java
+++ app/src/main/java/com/example/trainline/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.trainline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.loginBtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(LoginActivity.this,Dashboard.class);
+                startActivity(login);
+            }
+        });
     }
 }
