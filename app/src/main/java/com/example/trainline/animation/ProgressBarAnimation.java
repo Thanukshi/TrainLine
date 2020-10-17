be7c9e508b7f3f41fbb167bdaca8249fd56e92be
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-10-17 23:08:49 +0530

    Train Line (Thanukshi)

--- app/src/main/java/com/example/trainline/animation/ProgressBarAnimation.java
+++ app/src/main/java/com/example/trainline/animation/ProgressBarAnimation.java
@@ -1,4 +1,19 @@
 package com.example.trainline.animation;
 
-public class ProgressBarAnimation {
+import android.content.Context;
+import android.view.animation.Animation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class ProgressBarAnimation extends Animation {
+
+    //Declare the variable in main xml layout
+    private Context context;
+    private ProgressBar progressBar;
+    private TextView textView;
+    private float fromTime;
+    private float toTime;
+
+    //call 
+
 }
