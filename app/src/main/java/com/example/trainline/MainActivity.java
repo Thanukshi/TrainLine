f245e49d275058cbef99679bb2670d3227493d91
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-10-17 23:49:32 +0530

    Train Line (Thanukshi)

--- app/src/main/java/com/example/trainline/MainActivity.java
+++ app/src/main/java/com/example/trainline/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.trainline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -19,13 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         progressBar = findViewById(R.id.pBarWelcome);
         textView = findViewById(R.id.text1);
 
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressBarAnimation();
     }
 
     //Call the constructor of Progress
-    public void ProgressBarAnimation(){
+    public void progressBarAnimation(){
         ProgressBarAnimation barAnimation = new ProgressBarAnimation(this, progressBar, textView, 0f, 100f);
+        barAnimation.setDuration(5000);
+        progressBar.setAnimation(barAnimation);
     }
 }
