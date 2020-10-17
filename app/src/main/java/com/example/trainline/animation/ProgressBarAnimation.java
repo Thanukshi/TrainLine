7570336605cca887600f76242b73417125f2ed60
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-10-17 23:25:24 +0530

    Train Line (Thanukshi)

--- app/src/main/java/com/example/trainline/animation/ProgressBarAnimation.java
+++ app/src/main/java/com/example/trainline/animation/ProgressBarAnimation.java
@@ -1,7 +1,9 @@
 package com.example.trainline.animation;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -14,6 +16,21 @@ public class ProgressBarAnimation extends Animation {
     private float fromTime;
     private float toTime;
 
-    //call 
+    //declare the constructor bar function
+    public ProgressBarAnimation(Context context, AttributeSet attrs, Context context1, ProgressBar progressBar, TextView textView, float fromTime, float toTime) {
+        this.context = context;
+        this.progressBar = progressBar;
+        this.textView = textView;
+        this.fromTime = fromTime;
+        this.toTime = toTime;
+    }
 
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        super.applyTransformation(interpolatedTime, t);
+
+        float value = fromTime + (toTime - fromTime) * interpolatedTime;
+        progressBar.setProgress((int)value);
+        textView.setText((int)value + " %");
+    }
 }
