2c34219016f0a1272d0b2b49eb5ec132c1769780
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-10-18 11:19:02 +0530

    Train Line (Thanukshi)

--- app/src/main/java/com/example/trainline/Dashboard.java
+++ app/src/main/java/com/example/trainline/Dashboard.java
@@ -2,13 +2,28 @@ package com.example.trainline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
+
+    Button bookingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        bookingBtn = findViewById(R.id.booking);
+
+        bookingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent booking = new Intent(Dashboard.this, BookingActivity.class);
+                startActivity(booking);
+            }
+        });
     }
 }
