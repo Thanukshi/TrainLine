ea4d97b9864f8a7d836533408dc2302cecb877aa
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-10-18 14:35:32 +0530

    Train Line (Thanukshi)

--- app/src/main/java/com/example/trainline/TrainSchedule.java
+++ app/src/main/java/com/example/trainline/TrainSchedule.java
@@ -3,17 +3,12 @@ package com.example.trainline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class TrainSchedule extends AppCompatActivity {
-
-    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train_schedule);
-
-        back = findViewById(R.id.image1_ts);
     }
 }
