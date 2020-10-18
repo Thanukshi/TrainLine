5de129c15e3316789ae15afb5c78cfafd2893d36
Thanukshi <43212582+Thanukshi@users.noreply.github.com>
2020-10-18 13:07:43 +0530

    Train Line (Thanukshi)

--- app/src/main/java/com/example/trainline/AdminActivity.java
+++ app/src/main/java/com/example/trainline/AdminActivity.java
@@ -2,13 +2,28 @@ package com.example.trainline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminActivity extends AppCompatActivity {
+
+    Button trainSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        trainSchedule = findViewById(R.id.tSchedule);
+
+        trainSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scheduleTrain = new Intent(AdminActivity.this, TrainShcedule.class);
+                startActivity(scheduleTrain);
+            }
+        });
     }
 }
